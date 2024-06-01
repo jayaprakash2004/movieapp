e3ea606bc18426477313016850288df99fc3f681
jayaprakash2004 <jayaprakashsankula9393@gmail.com>
2024-06-01 16:50:19 +0530

    img done

--- src/pages/detailsBanner/DetailsBanner.jsx
+++ src/pages/detailsBanner/DetailsBanner.jsx
@@ -5,7 +5,7 @@ import dayjs from "dayjs";
 import "./style.scss";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import useFetch from "../../hooks/useFetch";
-import Img from "../../components/lazyLoadImage/Img";
+import Img from "../../components/lazyLoadImage/img";
 import PosterFallback from "../../assets/no-poster.jpg";
 import Genres from "../../components/genres/Genres";
 import CircleRating from "../../components/circleRating/CircleRating";
